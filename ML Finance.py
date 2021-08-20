81515bb96bf747e6c3c0dc6b49f5a9f18a367356
Rasikh Amirali <75446921+rasikhamirali@users.noreply.github.com>
2021-08-20 16:21:28 -0500

    Add files via upload
    Machine Learning Python Bot to track trends and predict tomorrow price of Netflix stock.

---
+++ ML Finance.py
@@ -0,0 +1,9 @@
+import numpy as np
+!pip install quandl
+!pip install pytrends
+import stocker
+from stocker import Stocker
+!git clone https://github.com/WillKoehrsen/Data-Analysis.git
+
+[ ] google = Stocker('GOOGL')
+[ ] google.plot_stock
